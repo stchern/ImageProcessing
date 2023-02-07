c57b8e5ef9ccf170f149c09fecf79bb74bafd36e
stchern <nastyachernyshovabbk00@mail.ru>
2023-02-07 20:54:02 +0300

    Apply otsu binarization; comparing with opencv binarization

--- ImageBinarization/ImageBinarizationProject/main.cpp
+++ ImageBinarization/ImageBinarizationProject/main.cpp
@@ -1,5 +1,6 @@
 #include "opencv2/core.hpp"
 #include <opencv2/opencv.hpp>
+#include "ImageBinarization/globalbinarization.h"
 
 #include <iostream>
 #include <string>
@@ -7,8 +8,7 @@
 int main() {    
 
     cv::samples::addSamplesDataSearchPath("../../images/");
-    const std::string image_path = cv::samples::findFile("images.png");
-    std::cout<< "image path: " << image_path << std::endl;
+    const std::string image_path = cv::samples::findFile("doc.png");
     const cv::Mat image = cv::imread(image_path, cv::IMREAD_ANYCOLOR);
 
     if (image.empty()) {
@@ -19,15 +19,23 @@ int main() {
 
     cv::Mat imageGray, imageBlur;
     cv::cvtColor(image, imageGray, cv::COLOR_BGR2GRAY);
-    cv::GaussianBlur(imageGray, imageBlur, cv::Size(7, 7), 5);
+    cv::GaussianBlur(imageGray, imageBlur, cv::Size(7, 7), 2);
 
+    float otsuThreshold = GlobalBinarization::OtsuMethod::bestThreshold(imageGray);
+    cv::Mat resultOtsuImage = GlobalBinarization::binarizeImageByThreshold(imageGray, otsuThreshold);
+    std::cout<< "threshold: " << otsuThreshold << std::endl;
 
+    cv::Mat resultCVOtsuImage;
+    double cv_threshold = cv::threshold(imageGray, resultCVOtsuImage, 0, 255, cv::THRESH_OTSU);
+    std::cout<< "cv_threshold: " << cv_threshold << std::endl;
 
     cv::imshow("Original image", image);
-    cv::imshow("Gray image", imageGray);
     cv::imshow("Blured image", imageBlur);
+    cv::imshow("Otsu binarized image", resultOtsuImage);
+    cv::imshow("Otsu CV binarized image", resultCVOtsuImage);
 
-    cv::imwrite("../../images/new_image.png", image);
+    cv::imwrite("../../images/otsu_image.png", resultOtsuImage);
+    cv::imwrite("../../images/otsu_cv_image.png", resultCVOtsuImage);
 
     cv::waitKey(0);
     return 0;
