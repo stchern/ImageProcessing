399868831ddd46e7777d8153c87ee43579b66a4b
stchern <nastyachernyshovabbk00@mail.ru>
2023-02-09 15:04:22 +0300

    Add comments for different binarization; add n_channels checking

--- ImageBinarization/ImageBinarizationProject/main.cpp
+++ ImageBinarization/ImageBinarizationProject/main.cpp
@@ -1,14 +1,15 @@
 #include "opencv2/core.hpp"
 #include <opencv2/opencv.hpp>
-#include "ImageBinarization/globalbinarization.h"
 
-#include <iostream>
+#include "ImageBinarization/globalbinarization.h"
+#include "ImageBinarization/localbinarization.h"
+
 #include <string>
 
 int main() {    
 
     cv::samples::addSamplesDataSearchPath("../../images/");
-    const std::string image_path = cv::samples::findFile("doc.png");
+    const std::string image_path = cv::samples::findFile("image.jpeg");
     const cv::Mat image = cv::imread(image_path, cv::IMREAD_ANYCOLOR);
 
     if (image.empty()) {
@@ -17,25 +18,28 @@ int main() {
         return -1;
     }
 
-    cv::Mat imageGray, imageBlur;
-    cv::cvtColor(image, imageGray, cv::COLOR_BGR2GRAY);
-    cv::GaussianBlur(imageGray, imageBlur, cv::Size(7, 7), 2);
+    // ------------------------------- preprocessing --------------------------------------------------------------
+    cv::Mat imageGray, imageGaussianBlur, imageMedianBlur;
+    if (image.channels() == 3)
+        cv::cvtColor(image, imageGray, cv::COLOR_BGR2GRAY);
+    else if (image.channels() == 1)
+        imageGray = image;
+    cv::GaussianBlur(imageGray, imageGaussianBlur, cv::Size(3, 3), 0);
+    cv::medianBlur(imageGray, imageMedianBlur, 3);
 
-    float otsuThreshold = GlobalBinarization::OtsuMethod::bestThreshold(imageGray);
-    cv::Mat resultOtsuImage = GlobalBinarization::binarizeImageByThreshold(imageGray, otsuThreshold);
-    std::cout<< "threshold: " << otsuThreshold << std::endl;
+    //------------------------------- global thresholding ---------------------------------------------------------
 
-    cv::Mat resultCVOtsuImage;
-    double cv_threshold = cv::threshold(imageGray, resultCVOtsuImage, 0, 255, cv::THRESH_OTSU);
-    std::cout<< "cv_threshold: " << cv_threshold << std::endl;
+    float otsuThreshold = GlobalBinarization::OtsuMethod::bestThreshold(imageGaussianBlur);
+    cv::Mat resultOtsuImage = GlobalBinarization::binarize(imageGaussianBlur, otsuThreshold);
 
-    cv::imshow("Original image", image);
-    cv::imshow("Blured image", imageBlur);
-    cv::imshow("Otsu binarized image", resultOtsuImage);
-    cv::imshow("Otsu CV binarized image", resultCVOtsuImage);
+    //------------------------------- local thresholding ----------------------------------------------------------
+    const int windowSize = 5;
+    const int contrastLimit = 15;
+    cv::Mat resultBernsenImage = LocalBinarization::BernsenMethod::binarize(imageMedianBlur, windowSize, contrastLimit);
 
-    cv::imwrite("../../images/otsu_image.png", resultOtsuImage);
-    cv::imwrite("../../images/otsu_cv_image.png", resultCVOtsuImage);
+    // -----------------------------------------------------------------------------------------------------------
+    cv::imwrite("../../images/global_otsu_image.jpg", resultOtsuImage);
+    cv::imwrite("../../images/local_bernsen_image.jpg", resultBernsenImage);
 
     cv::waitKey(0);
     return 0;
